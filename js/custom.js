675bc1923720ac872ee693ba7706aea01d3941e7
hashimmunawar3-jpg <hashimmunawar@gmail.com>
2025-12-08 14:40:19 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,78 @@
+// AOS Initializaion
+AOS.init({duration:1000,once:true });
+
+//menu togge
+function toggleMenu(){
+    document.getElementById('menu').classList.toggle('active');
+}
+
+function animationCounter(id,target,speed){
+    let count = 0;
+    const interval = setInterval(()=>{
+        if(count < target){
+            count++;
+            document.getElementById(id).innerText = count;
+        }else{
+            clearInterval(interval);
+        }
+    }, speed);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    animationCounter('courseCount', 50, 40);
+    animationCounter('studentCount', 210, 10);
+    animationCounter('PlacementRate', 95, 20);
+});
+// testimonial slider
+
+const wrapper = document.getElementById('testimonialwrapper');
+const dots = document.querySelectorAll('.dot');
+let currentSlide = 0;
+const totalSlides = 2;
+
+function moveToSlide(index){
+    currentSlide = index;
+    const slideWidth = wrapper.clientWidth;
+    wrapper.style.transform = `translateX(-${index * 100}%)`;
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[index].classList.add('active');
+}
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    moveToSlide(currentSlide)
+},5000);
+ 
+// Scroll Events
+window.addEventListener("scroll", () => {
+    const scrollY = window.scrollY;
+    const header = document.getElementById("mainHeader");
+    const topBtn = document.getElementById("backToTop");
+
+    if (header) {
+        header.classList.toggle("fixed", scrollY > 100);
+    }
+
+    if (topBtn) {
+        topBtn.style.display = scrollY > 300 ? "block" : "none";
+    }
+});
+
+ 
+
+document.querySelectorAll('.scrollLink').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetID = this.getAttribute('href');
+        const headerHeight = document.querySelector('#mainHeader').offsetHeight;
+        const target = document.querySelector(targetID);
+
+        window.scrollTo({
+            top: target.offsetTop - headerHeight,
+            behavior: 'smooth'
+        });
+
+        // Mobile menu auto close
+        document.getElementById('menu').classList.remove('active');
+    });
+});
